ce4201f279d135a1a24696ed62b8e8a888ac10d3
KooHyeonwoo <koo999@wafflestudio.com>
2018-11-08 16:05:01 +0900

    Register video from loudest point

--- register.py
+++ register.py
@@ -7,19 +7,27 @@ class Register(object):
     def __init__(self, path=None, note='c3', *args, **kwargs):
         self.note = note
         self.path = None
+        self.register(path)
+
+    def register(self, path):
         if os.path.isfile(path):
             print("Register : " + path + " is existed")
             _, ext = os.path.splitext(path)
-            self.path = './note/'+note+'.mp4'
+            self.path = './note/'+self.note+'.mp4'
             sample = VideoFileClip(path)
-            sample.resize((720,480))
+            sample.resize((720, 480))
 
             volume = 0.5
             audio = AudioFileClip(path)
             audio_array = audio.to_soundarray()
-            scale = volume / max(audio_array[:, 0])
+            audio_array = audio_array[:, 0].tolist()
+            max_sound = max(audio_array)
+            loudest_index = audio_array.index(max_sound)
+            scale = volume / max_sound
             audio = audio.fx(volumex, scale)
+
             sample = sample.set_audio(audio)
+            sample = sample.subclip(loudest_index/44100, sample.end)
             sample.write_videofile(self.path)
         else:
             print("Register : Wrong path")
